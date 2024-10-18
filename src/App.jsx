dbf5f1a5b66b58274ef3766b7d37ac43618016b6
Icaro Jordano <Icarojordano>
2024-10-18 06:59:20 -0300

    feat: section sobre mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Header } from "./Header"
 import { Home } from "./Home"
 import { Explore } from "./Explore"
 import {register} from 'swiper/element/bundle'
+import { Sobre } from "./Sobre"
 
 register()
 function App() {
 
   return (
-    <div className="p-0 m-0 bg-neutral-100">
+    <div className="pb-10 m-0 bg-neutral-100">
       <Header></Header>
       <Home></Home>
       <Explore></Explore>
+      <Sobre></Sobre>
     </div>
   )
 }
--- src/Explore.jsx
+++ src/Explore.jsx
@@ -5,10 +5,10 @@ import "swiper/css"
 import { SlideExplore } from './SlideExplore'
 export function Explore() {
     return(
-        <section className="py-20" >
-            <h2 className="font-serif  text-3xl text-neutral-700/80 ml-5   font-thin">Conheca os Produtos</h2>
-            <a className="font-bold ml-5 text-neutral-700 underline flex items-center"  href="">Comprar <FiExternalLink className="ml-1"></FiExternalLink></a>
-            <Swiper className='py-5 pl-5' spaceBetween={0} slidesPerView={1.2}  >
+        <section className="py-20 pl-5" >
+            <h2 className="font-serif  text-3xl text-neutral-700/80  font-thin">Conheca os Produtos</h2>
+            <a className="font-bold text-neutral-700 underline flex items-center"  href="">Comprar <FiExternalLink className="ml-1"></FiExternalLink></a>
+            <Swiper className='py-5' spaceBetween={0} slidesPerView={1.2}  >
                 <SwiperSlide><SlideExplore></SlideExplore></SwiperSlide>
                 <SwiperSlide><SlideExplore></SlideExplore></SwiperSlide>
                 <SwiperSlide><SlideExplore></SlideExplore></SwiperSlide>

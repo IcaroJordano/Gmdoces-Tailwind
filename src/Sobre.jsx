003c5059a1c8f7a607933d58e71bddd866b45aaa
Icaro Jordano <Icarojordano>
2024-10-20 02:53:05 -0300

    wip

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -2,7 +2,7 @@ import { FiExternalLink } from "react-icons/fi";
 
 export function Sobre() {
     return(
-        <section className="pl-5">
+        <section className="py-10 pl-5">
             <h2 className="text-2xl text-neutral-600 font-serif" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil saepe, cum architecto obcaecati ratione culpa sunt ea cumque illo, laboriosam hic impedit quisquam aliquid eos quia eum et ut. Incidunt.</h2>
             <a className="font-bold mt-3 text-neutral-700 underline flex items-center"  href="">Leia nossa historia <FiExternalLink className="ml-1"></FiExternalLink></a>
             <img className="w-11/12 h-56 pt-5" src="https://images.pexels.com/photos/2072152/pexels-photo-2072152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
